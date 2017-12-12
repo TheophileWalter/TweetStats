6ec0d35b0eb2fd6c7c86318a691e269bb6d81b9a
TheophileWalter <theophilewalter@gmail.com>
2017-12-12 13:41:00 +0100

    Added CustomHandler in the SimpleHTTPServer

--- back/server.py
+++ back/server.py
@@ -15,31 +15,42 @@ import atexit
 import SimpleHTTPServer
 import SocketServer
 
+
 # Get the parameters
 if sys.argv[1:]:
     port = int(sys.argv[1])
 else:
     port = 2319
 
+
 # Set the correct directory
 back_path = os.path.dirname(os.path.abspath(__file__))
 root_path = os.path.dirname(back_path)
 front_path = root_path + '/front'
-print 'Going to ', front_path
+print ('Going to ' + front_path)
 os.chdir(front_path)
 
+
+# A handler for the HTTP requests
+class CustomHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    def __init__(self, req, client_addr, server):
+        SimpleHTTPServer.SimpleHTTPRequestHandler.__init__(self, req, client_addr, server)
+    def do_GET(self):
+        # Handle here the request with self.path
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
 # Creates a simple HTTP server
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(('localhost', port), Handler)
+httpd = SocketServer.TCPServer(('localhost', port), CustomHandler)
 
 # Close the socket on exit
-# TODO: Don't work at all times :/
+# TODO: Does not works all times
 def close_socket():
-    print 'closing socket.'
+    print ('closing socket.')
     httpd.server_close()
 
 atexit.register(close_socket)
 
 # Run the server
-print 'serving at port', port
+print ('serving at port ' + str(port))
 httpd.serve_forever()
